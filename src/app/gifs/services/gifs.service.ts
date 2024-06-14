2f55e5441090b1f8f0daed0616d057a98828dc00
borishdez <borisnandez93@gmail.com>
2024-06-14 15:48:56 -0600

    validacion de repeticion de tag

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -8,9 +8,30 @@ export class GifsService {
     return [...this._tagsHistory];
   }
 
-  //agrega tag al principio del array para guardarlo en ese array
-  public searchTag( tag:string ):void {
+  private organizeHistory(tag: string) {
+    //graba en minuscula pero pipe sigue desplegando ordenado
+    tag = tag.toLowerCase();
+
+    if ( this.tagsHistory.includes(tag) ) {
+      //borra tag que cumple la condicion
+      //es decir los tags que ya estan almacenados que sean iguales
+      //si son diferentes los deja pasar
+      this._tagsHistory = this._tagsHistory.filter( (oldTag) => oldTag !== tag )
+    }
+
+    //agrega nuevo tag al inicio
     this._tagsHistory.unshift( tag );
   }
+
+  //agrega tag al principio del array para guardarlo en ese array
+  public searchTag( tag:string ):void {
+    //si no escibre no pasa nada
+    if ( tag.length === 0 ) return;
+
+    this.organizeHistory(tag);
+
+    this._tagsHistory.unshift( tag );
+  }
+
 
 }
