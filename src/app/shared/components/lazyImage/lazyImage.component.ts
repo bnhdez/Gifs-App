657ffc2d73a5a739fad18c68b14e1b865c033050
borishdez <borisnandez93@gmail.com>
2024-06-17 11:42:16 -0600

    declaracion url

--- src/app/shared/components/lazyImage/lazyImage.component.ts
+++ src/app/shared/components/lazyImage/lazyImage.component.ts
@@ -1,7 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
     selector: 'shared-lazy-image',
     templateUrl: './lazyImage.component.html',
 })
-export class LazyImageComponent { }
+export class LazyImageComponent implements OnInit {
+  @Input()
+  public url!:string;
+
+  ngOnInit(): void {
+    if ( !this.url ) throw new Error('URL property is required');
+  }
+}
