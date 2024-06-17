4e027c69eabfaa0d123168e147c22cfbbc665ef6
borishdez <borisnandez93@gmail.com>
2024-06-17 11:31:53 -0600

    component lazyImage creado

---
+++ src/app/shared/components/lazyImage/lazyImage.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'shared-lazy-image',
+    templateUrl: './lazyImage.component.html',
+})
+export class LazyImageComponent { }
